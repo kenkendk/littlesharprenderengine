91e4fdae414c67d0e47e9e7385cc77cfef6928dd
removedporn <86824510+removedporn@users.noreply.github.com>
2021-11-07 21:21:19 +0800

    Delete LittleSharpRenderEngine directory

--- LittleSharpRenderEngine/Style/IColoredItem.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace LittleSharpRenderEngine.Style
-{
-    public interface IColoredItem
-    {
-        System.Drawing.Color BackgroundColor { get; set; }
-        System.Drawing.Color ForegroundColor { get; set; }
-    }
-}
--- LittleSharpRenderEngine/Style/IFill.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace LittleSharpRenderEngine.Style
-{
-    public interface IFill : IColoredItem
-    {
-        System.Drawing.Drawing2D.HatchStyle HatchStyle { get; set; }
-        System.Drawing.Image Pattern { get; set; }
-    }
-}
